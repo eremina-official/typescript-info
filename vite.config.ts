8f6eb1c51b30e6764008aa029e9ff649610dac49
Marina Eremina <eremina.jobmail@gmail.com>
2025-08-08 15:51:37 +0200

    Add vite plugin svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { basePath } from './src/constants';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgrPlugin({
+      include: '**/*.svg?react',
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
